8470953d9977cb4688a1c8f9c2b05580b17751d8
alexbanaga <alexbanaga619@gmail.com>
2017-12-07 16:56:02 -0800

    first push

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import Homepage from './scenes/Home/index'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Homepage />
+      </div>
+    );
+  }
+}
+
+export default App;
+
